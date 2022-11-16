f944c0acd2089789681de6b2bba866ed8dbef340
augustin <augustin.agm@gmail.com>
2022-11-16 18:36:19 +0100

    réarrange data

--- twitter_app/data.py
+++ twitter_app/data.py
@@ -1,7 +1,7 @@
 from . import db
 from flask import Blueprint, jsonify, request
 
-user = Blueprint('user', __name__)
+data = Blueprint('data', __name__)
 
 class User(db.Model):
     __tablename__ = "user"
@@ -11,7 +11,15 @@ class User(db.Model):
     password = db.Column(db.String(64))
     tweets = db.relationship("Tweet", cascade="all, delete")
     
-@user.route("/api/users", methods=["GET", "POST", "DELETE"])
+class Tweet(db.Model):
+    __tablename__ = "tweet"
+    id = db.Column(db.Integer, primary_key=True)
+    uid = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    title = db.Column(db.String(256))
+    content = db.Column(db.String(2048))
+    date = db.Column(db.Date)
+    
+@data.route("/users", methods=["GET", "POST", "DELETE"])
 def users(user_id = 0):
     if request.method == 'GET':
         users = User.query.all()
@@ -44,7 +52,7 @@ def users(user_id = 0):
         db.session.commit()
         return jsonify({}), 200
     
-@user.route("/api/users/follows", methods=["GET", "POST", "DELETE"])
+@data.route("/api/users/follows", methods=["GET", "POST", "DELETE"])
 def follows(user_id, follows):
     if request.method == 'GET':#We will use a hashmap in the form of python sets to get fast access in average O(1)
         pass
@@ -52,3 +60,37 @@ def follows(user_id, follows):
         pass
     if request.method == 'DELETE':
         pass
+    
+    
+    
+data.route("/api/tweets", methods=["GET", "POST", "DELETE"])
+def tweets(tweet_id = 0):
+    if request.method == 'GET':
+        tweets = Tweet.query.all()
+        all_tweets = []*len(tweets) #autre data structure?
+        for tweet in tweets:
+            all_tweets[tweet.id] = {
+                'id':tweet.id,
+                'uid':tweet.uid, 
+                'title':tweet.title,
+                'content':tweet.content,
+                'date':tweet.date
+                }
+        tweet = tweets[tweet_id]
+        return jsonify(tweet), 200
+    #alternative data structure: hashmap gives complexity average in O(1            )
+    if request.method == 'POST':
+        data = request.get_json()
+        new_tweet = Tweet(
+                title = data['title'],
+                content = data['content'],
+                uid = data['user']
+        )
+        db.session.add(new_tweet)
+        db.session.commit()
+        return jsonify({"message": "Tweet posted"}), 200
+    if request.method == 'DELETE':
+        tweet = Tweet.query.filter_by(id = tweet_id),first()
+        db.session.delete(tweet)
+        db.session.commit()
+        return jsonify({}), 200
